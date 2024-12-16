ebef06a348d88927eb382f40b36b179a3616400b
anandprakash01 <ap.anandprakash21@gmail.com>
2024-12-16 21:39:26 +0530

    chat.jsx changes

--- server/index.js
+++ server/index.js
@@ -43,6 +43,7 @@ app.use(errorHandler);
 
 // ========================Deployment===================
 
+console.log(process.env.NODE_ENV);
 const __dirname1 = path.resolve(); //current working dir
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/client/dist")));
