4f3e7f59f9fe4a61d64aaabf15e4362cf88503d1
anandprakash01 <ap.anandprakash21@gmail.com>
2024-12-16 21:37:47 +0530

    chat.jsx changes

--- server/index.js
+++ server/index.js
@@ -49,6 +49,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname1, "client", "dist", "index.html"));
   });
+  console.log("hey");
 }
 
 // ==================================================
