3c14eb9a8d21d904cd5568fe327ee63ea943bdeb
anandprakash01 <ap.anandprakash21@gmail.com>
2025-03-04 07:57:07 +0530

    updating login methods and responsivness

--- client/src/components/LoadingPage.jsx
+++ client/src/components/LoadingPage.jsx
@@ -1,9 +1,9 @@
 import loadingIcon from "../assets/loading.svg";
 
-const LoadingPage = ({width = 12}) => {
+const LoadingPage = () => {
   return (
     <h1 className="fixed z-50 top-0 left-0 bottom-0 right-0 flex items-center justify-center bg-bg_primary_dark opacity-70">
-      <img src={loadingIcon} alt="" className={`w-${width}`} />
+      <img src={loadingIcon} alt="" className={`w-12`} />
     </h1>
   );
 };
